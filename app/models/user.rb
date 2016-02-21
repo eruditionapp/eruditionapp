a27227e69b5435cbac740fdec071a8ef044ea2d8
findandrew <mail@andrewjm.co>
2016-02-20 19:02:52 -0600

    added roles to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+  enum role: [:free, :paid, :overdue, :admin, :superadmin]
+  after_initialize :set_role, if: :new_record?
+
+  def set_role
+    self.role ||= :user_free
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
