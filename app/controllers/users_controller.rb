b95740972b2a442ba626e45b35ad60019a949d80
findandrew <mail@andrewjm.co>
2016-02-22 10:46:13 -0600

    Refactor, all access-control as before_actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :admin_only, except: :show
+  before_action :self_or_admin_only, only: :show
 
   def index
     @users = User.all
@@ -8,17 +9,15 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    unless (@user == current_user) || current_user.is_admin?
-      deny_access
-    end
   end
 
   private
 
   def admin_only
-    unless current_user.is_admin?
-      deny_access
-    end
+    deny_access unless current_user.is_admin?
   end
 
+  def self_or_admin_only
+    deny_access unless (current_user == User.find(params[:id])) || current_user.is_admin?
+  end
 end
