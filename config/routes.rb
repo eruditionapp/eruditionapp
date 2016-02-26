df4dddf4c5402d0c8ee02862134ec9146271675f
cstillitano <cs@largetail.com>
2016-02-25 21:47:07 -0500

    Add privacy page, Update readme

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   root      'static#home'
 
+  get 'privacy' => 'static#privacy'
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
