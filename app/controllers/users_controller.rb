02de6fde9b1c7a8683e4a41b01dc338b567d869a
findandrew <mail@andrewjm.co>
2016-03-02 01:00:26 -0600

    superadmin can edit roles, seed data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :superadmin_only, only: :index
   before_action :admin_only, except: :show
   before_action :self_or_admin_only, only: :show
 
@@ -11,7 +12,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes secure_params
+      redirect_to users_path, alert: "User #{@user.email} updated."
+    else
+      redirect_to users_path, alert: 'Unabled to update user.'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path, alert: 'User deleted.'
+  end
+
   private
+
+  def superadmin_only
+    deny_access unless current_user.is_superadmin?
+  end
 
   def admin_only
     deny_access unless current_user.is_admin?
@@ -19,5 +38,9 @@ class UsersController < ApplicationController
 
   def self_or_admin_only
     deny_access unless (current_user == User.find(params[:id])) || current_user.is_admin?
+  end
+
+  def secure_params
+    params.require(:user).permit(:role)
   end
 end
