a80570d0db622688eea1bd9bfa0df26fe5bb9cdd
findandrew <mail@andrewjm.co>
2016-03-02 01:08:37 -0600

    Updating access control

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :superadmin_only, only: :index
-  before_action :admin_only, except: :show
-  before_action :self_or_admin_only, only: :show
+  before_action :superadmin_only,    only: [:index, :delete]
+  before_action :self_or_admin_only, only: [:show, :update, :edit]
 
   def index
     @users = User.all
@@ -10,6 +9,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def edit
   end
 
   def update
