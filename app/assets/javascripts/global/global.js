4987e91acf46d6f63c83c6c623afe03904c9703a
cstillitano <cs@largetail.com>
2016-03-17 21:56:06 -0400

    Format single deck view, fix for #11

--- app/assets/javascripts/global/global.js
+++ app/assets/javascripts/global/global.js
@@ -3,3 +3,9 @@
  *
  */
 var querySelector = document.querySelector.bind(document);
+
+
+// Hack for Turbolinks and Bootstrap Sprockets binding issue
+$(document).ready(function(){
+  $('.dropdown-toggle').dropdown();
+});
