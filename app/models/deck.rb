02a921046ad3b23ffb0559ba8a2bc72ed84a9c06
findandrew <mail@andrewjm.co>
2016-03-19 15:21:29 -0500

    add tier enums to deck

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -4,11 +4,17 @@ class Deck < ActiveRecord::Base
 
   validates_presence_of :title, :author
 
-  enum status: { free: 0, paid: 1 }
+  enum tier:   { free: 0, paid: 1 }
+  enum status: { published: 0, unpublished: 1 }
 
+  after_initialize :set_tier, if: :new_record?
   after_initialize :set_status, if: :new_record?
 
+  def set_tier
+    self.tier ||= :paid
+  end
+
   def set_status
-    self.status ||= :paid
+    self.status ||= :unpublished
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,10 +19,11 @@ end
 
 10.times do |count|
   Deck.create! title: "Deck #{count + 1}", subtitle: "Subtitle #{count + 1}",
-               author: "Author #{count + 1}"
+               author: "Author #{count + 1}", status: rand(0...Deck.statuses.count),
+               tier: rand(0...Deck.tiers.count)
 
   2.times do
-    category_id = rand(1...Category.count)
+    category_id = rand(1..Category.count)
     unless Deck.last.categories.where(id: category_id).exists?
       Deck.last.categories << Category.find(category_id)
     end
