7e4f13f3740b3d947f230aca62553d7054089d94
findandrew <mail@andrewjm.co>
2016-04-10 23:10:26 -0500

    filter decks by status, tier, and category

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,7 +1,13 @@
 class DecksController < ApplicationController
 
   def index
-    @decks = Deck.paginate(page: params[:page]).order(:title)
+    if params[:category].present?
+      @decks = Category.filter_decks(params[:category]).paginate(page: params[:page])
+    else
+      @decks = Deck.paginate(page: params[:page]).order(:title)
+      @decks = @decks.scope_tier(params[:tier])     if params[:tier].present?
+      @decks = @decks.scope_status(params[:status]) if params[:status].present?
+    end
     @categories = Category.all
   end
 
